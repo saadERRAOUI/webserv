9e2c850896732971b1b10a0feed4f0da5d3a8742
hicham bouzid <bouzidhicham51@gmail.com>
2025-02-13 17:14:19 +0100

    PUSH-0.0.3: update setup server .

--- SetupServer/StartServerSetup.cpp
+++ SetupServer/StartServerSetup.cpp
@@ -1,46 +1,52 @@
 #include "includes.hpp"
 
 /*
-	Author		: Hicham BOUZID
-	Date		: 2025-01-27
-	Description : initial function that set up Server.
+	Author: BOUZID Hicham
+	Description: set up server and make it ready to reach connections
+	Date: 2025-02-07
 */
-void setup()
+
+void SetUpServer()
 {
-	int fd_sock = socket(AF_INET, SOCK_STREAM, 0);
-	sockaddr_in address;
-	int reuse = 1;
-	memset(&address, 0, sizeof(sockaddr));
-	address.sin_family = AF_INET;
-	address.sin_port = htons(80);
-	// address.sin_addr.s_addr = inet_addr("127.0.0.1");
-	address.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (setsockopt(fd_sock, SOL_SOCKET, SO_REUSEPORT , &reuse, sizeof(int)))
-		std::cerr << "setsockopt(SO_REUSPORT) failed" << "\n";
-	std::cout << "-----> ::::: " << fd_sock << "\n";
-	int Rt_Bind = bind(fd_sock, (sockaddr *)&address, sizeof(address));
-	if (Rt_Bind == -1)
-		std::cerr << "Error happen with : " << strerror(errno) << "\n";
-	if (listen(fd_sock, 2) == -1)
-		std::cerr << "can not make a socket passive to accept connection\n";
-	std::cout << "our webserve is ready now ....\n";
-	struct sockaddr_in client_addr;
-	socklen_t len_client = sizeof(client_addr);
-	int client_sock = accept(fd_sock, (sockaddr *)&client_addr, &len_client);
-	if (client_sock == -1)
-		std::cout << "Error cannot connect: " << strerror(errno) << '\n';
-	char buffer[2024] = {0};
-	read(client_sock, buffer, 2024);
-	printf("%s", buffer);
-	write(client_sock, "hello world\n", sizeof("hello world\n"));
-	close(fd_sock);
-	close(client_sock);
-	close(address.sin_port);
+	int fdsocket;
+	struct sockaddr_in my_addr;
+	int const enable = 1;
+
+	fdsocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	std::cout << fdsocket << " it's fd socket \n";
+	if (fdsocket < 0)
+		std::cerr << "Sock Error: " << strerror(errno) << "\n";
+	memset(&my_addr, 0, sizeof(sockaddr));
+	my_addr.sin_family = AF_INET;
+	my_addr.sin_port = htons(8080); // specifie the port to be used .
+	my_addr.sin_addr.s_addr  = htonl(INADDR_ANY); // that mean any ip address can make a request to server .
+	if (setsockopt(fdsocket, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)))
+		std::cerr << "Error: " << strerror(errno) << "\n";
+	if (bind(fdsocket, (sockaddr *)&my_addr, sizeof(my_addr)))
+		std::cerr <<  "Binding error: " << strerror(errno) << "\n";
+	if (listen(fdsocket, 2))
+		std::cerr << "Error listen: " << strerror(errno) << "\n";
+	socklen_t len_client = sizeof(my_addr);
+	while (1)
+	{
+	int rt = accept(fdsocket, (sockaddr *)&my_addr, &len_client);
+	char buffer[1024] = {0};
+		if (rt < 0)
+			std::cout << "Error accept: " << strerror(errno) << '\n';
+		read(rt, buffer, 1024);
+		std::cout << std::string(buffer) << "\n";
+		const char *message = "hello world\n";
+		int n = write(rt, message, strlen(message));
+		std::cout << "Error: " << strerror(errno) << "\n";
+		std::cout << "----> n: " << n << "\n";
+	}
+	std::cout << "out of loop\n";
+
+	close(fdsocket);
+	close(my_addr.sin_port);
 }
 
-// next step binding this socket with port /
-// next step printing the content of  address struct and then move to under strund how bind work
-int main ()
+int main()
 {
-	setup();
+	SetUpServer();
 }
--- SetupServer/includes.hpp
+++ SetupServer/includes.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <sys/types.h>
 #include <sys/socket.h>
-#include <iostream>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <errno.h>
+#include <arpa/inet.h>
 #include <unistd.h>
+// #include <stdio.h>
 
+#include <iostream>
