3b12ff885be54b244ed88e2798de61164f7e6a51
hicham bouzid <bouzidhicham51@gmail.com>
2025-04-27 15:54:32 +0100

    SWITCH device

--- HttpResponse/GetMethod.cpp
+++ HttpResponse/GetMethod.cpp
@@ -20,7 +20,7 @@ bool CompareRU(std::string URI, std::string location){
     }
     if (index == (int)tmplocation.length())
         return (true);
-    return (false);    
+    return (false);
 }
 
 
@@ -40,7 +40,7 @@ bool MethodAllowed(std::vector<std::string> list, std::string method)
 /*
     Author: BOUZID Hicham
     Description: match the request That we got with ours routes
-    Date: 2025-04-24 
+    Date: 2025-04-24
 */
 
 std::string MatchRoutes(std::map<std::string, route> &TmpRoutes, HttpRequest &TmpRequest){
@@ -50,22 +50,19 @@ std::string MatchRoutes(std::map<std::string, route> &TmpRoutes, HttpRequest &Tm
     for (it = TmpRoutes.begin();it != TmpRoutes.end(); it++){
         if (CompareRU(TmpRequest.getRequestURI(), it->first) == true){
             if(MethodAllowed(TmpRoutes[it->first].getMethods(), "GET") == false)
-            {
-                //SERVE ERROR HERE
-            }
-            // if true use the route and check if the method allowed or not
-            // if allowed check if directory send 301 error to http
+                return (std::string("405"));
+            else
+                return (it->first);
         }
     }
-
-    return(std::string("ROUTE NOT FOUND TO SERVE !!!"));
+    return(std::string("404"));
 }
 
 
 
 /*
     Author: BOUZID Hicham
-    Description: Implementing GET method that 
+    Description: Implementing GET method that
                 simulate other webserves like nginx or apache
     Date: 2025-04-24
 */
